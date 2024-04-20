ee2a25cfa4141f3ef8deb6f118770df176551c71
Johnny Trinh <johnny.trinh@hotmail.se>
2024-04-21 00:18:19 +0200

    Add comments with DjangoFilterBackend

--- comments/views.py
+++ comments/views.py
@@ -1,17 +1,22 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from coachAPI.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
 
 class CommentList(generics.ListCreateAPIView):
+    # List all comments or create a new comment
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['review']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    # Retrieve, update or delete a comment
     serializer_class = CommentDetailSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Comment.objects.all()
