cb6ab45fd04f988fe071c0aede43aef3cb963eb9
Johnny Trinh <johnny.trinh@hotmail.se>
2024-04-20 05:16:13 +0200

    Add FollowerList and Details generics

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from coachAPI.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+class FollowerList(generics.ListCreateAPIView):
+    # List all followers or create a new follower.
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class FollowerDetail(generics.RetrieveUpdateDestroyAPIView):
+    # Retrieve, update or delete a follower.
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+    permission_classes = [IsOwnerOrReadOnly]
