da750f530cdc7b548297b49767a20d9ca400d1f6
Johnny Trinh <johnny.trinh@hotmail.se>
2024-04-20 06:16:48 +0200

    Refactor: Profile and Post views to generic views

--- profiles/views.py
+++ profiles/views.py
@@ -1,52 +1,20 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
+from coachAPI.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
-from coachAPI.permissions import IsOwnerOrReadOnly
 
-
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles,
-            many=True,
-            context={'request': request}
-        )
-        return Response(serializer.data)
-    
-class ProfileDetail(APIView):
-    permission_classes = [IsOwnerOrReadOnly]
+class ProfileList(generics.ListCreateAPIView):
+    """
+    List all profiles.
+    No create view as profile creation is handled by the User model.
+    """
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
 
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile,
-            context={'request': request}
-        )
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile,
-            data=request.data,
-            context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update, or delete a profile.
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
