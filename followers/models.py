91f2f4509be81c036a460b8dc521f19d40872c35
Johnny Trinh <johnny.trinh@hotmail.se>
2024-04-20 05:06:10 +0200

    Update followers models and create followers serializers

--- followers/models.py
+++ followers/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Follower(models.Model):
+    """
+    Followers model, related to 'owner' and 'followed'.
+    'owner' is the user who is following another user.
+    'followed' is the user who is being followed.
+    We need to set 'related_name' to 'followers' so that 
+    we can access the followers of a user.
+    """
+    owner = models.ForeignKey(
+      User,
+      on_delete=models.CASCADE,
+      related_name='following'
+    )
+    followed = models.ForeignKey(
+      User,
+      on_delete=models.CASCADE,
+      related_name='followers'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['owner', 'followed']
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f'{self.owner} {self.followed}'
